a262d0ea982a1fb53600ce5fc4a6fad9e83c6095
taxintt <nthouse14@gmail.com>
2023-02-28 14:09:19 +0900

    fix: comment out unneeded fmt.Println

--- app/main.go
+++ app/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 )
@@ -9,7 +8,7 @@ import (
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	// fmt.Println("Logs from your program will appear here!")
 
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
@@ -17,7 +16,6 @@ func main() {
 	ps, err := exec.Command(command, args...).CombinedOutput()
 	if err != nil {
 		// write to stdout
-		fmt.Println(err.Error())
 		os.Stderr.Write([]byte(err.Error()))
 	}
 
