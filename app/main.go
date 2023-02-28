4c17127d216c0ca151ee4cb9807b068f032d2bd3
taxintt <nthouse14@gmail.com>
2023-02-28 14:23:17 +0900

    feat: pipe stdout/err of process to os.stdout/err

--- app/main.go
+++ app/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"os"
 	"os/exec"
 )
@@ -8,17 +10,23 @@ import (
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	// fmt.Println("Logs from your program will appear here!")
+	fmt.Println("Logs from your program will appear here!")
 
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
+	cmd := exec.Command(command, args...)
 
-	ps, err := exec.Command(command, args...).CombinedOutput()
+	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		// write to stdout
-		os.Stderr.Write([]byte(err.Error()))
+		fmt.Println(err.Error())
+		os.Exit(1)
 	}
+	io.Copy(os.Stdout, stdout)
 
-	// write to stdout
-	os.Stdout.Write(ps)
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	io.Copy(os.Stderr, stderr)
 }
