0c253bb5154e35bcdf429f280dd1df87548cdb29
taxintt <nthouse14@gmail.com>
2023-02-28 14:50:10 +0900

    fix typo

--- app/main.go
+++ app/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 )
@@ -9,7 +8,7 @@ import (
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	// fmt.Println("Logs from your program will appear here!")
 
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
