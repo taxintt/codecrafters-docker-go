9e858f1d3354d012452027e87c67762f9be323e8
taxintt <nthouse14@gmail.com>
2023-03-07 22:35:52 +0900

    copy executable file to chroot dir

--- app/main.go
+++ app/main.go
@@ -4,8 +4,10 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path"
 	"path/filepath"
 	"syscall"
 )
@@ -26,11 +28,26 @@ func setupOutput(cmd *exec.Cmd) {
 	go io.Copy(os.Stderr, stderr)
 }
 
-func createDevNull() {
-	// workaround for chroot
-	os.Mkdir(filepath.Join("mydocker", "dev"), os.ModeDir)
-	devnull, _ := os.Create(filepath.Join("mydocker", "/dev/null"))
-	devnull.Close()
+func copyExecutablePath(source, dest string) error {
+	sourceFileStat, err := os.Stat(source)
+	if err != nil {
+		return err
+	}
+
+	sourceFile, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+
+	destinationFile, err := os.OpenFile(dest, os.O_RDWR|os.O_CREATE, sourceFileStat.Mode())
+	if err != nil {
+		return err
+	}
+	defer destinationFile.Close()
+
+	_, err = io.Copy(destinationFile, sourceFile)
+	return err
 }
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
@@ -43,12 +60,21 @@ func main() {
 	cmd := exec.Command(command, args...)
 	setupOutput(cmd)
 
-	if err := os.MkdirAll(filepath.Join("mydocker", filepath.Dir(command)), os.ModeDir); err != nil {
+	// create executable path (e.g. /usr/local/bin/docker-explorer)
+	// https://text.baldanders.info/golang/deprecation-of-ioutil/
+	chrootDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		fmt.Printf("error creating chroot dir: %v", err)
 		os.Exit(1)
 	}
-	defer os.RemoveAll("mydocker")
+	if err := os.MkdirAll(filepath.Join(chrootDir, command), os.ModeDir); err != nil {
+		os.Exit(1)
+	}
+	copyExecutablePath(command, chrootDir)
 
-	createDevNull()
+	// workaround for chroot
+	os.Mkdir(filepath.Join(chrootDir, "dev"), os.ModeDir)
+	ioutil.WriteFile(path.Join(chrootDir, "dev", "null"), []byte{}, 0644)
 
 	// chroot
 	syscall.Chroot(args[1])
