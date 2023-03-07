a78d3200d14cfc3966965209a295313d8b7d70d9
taxintt <nthouse14@gmail.com>
2023-03-07 22:44:38 +0900

    debug

--- app/main.go
+++ app/main.go
@@ -73,11 +73,11 @@ func main() {
 	copyExecutablePath(command, chrootDir)
 
 	// workaround for chroot
-	os.Mkdir(filepath.Join(chrootDir, "dev"), 0750)
 	if err := os.MkdirAll(path.Join(chrootDir, "dev"), 0750); err != nil {
 		os.Exit(1)
 	}
-	ioutil.WriteFile(path.Join(chrootDir, "dev", "null"), []byte{}, 0644)
+	devnull, _ := os.Create(filepath.Join(chrootDir, "/dev/null"))
+	devnull.Close()
 
 	// chroot
 	syscall.Chroot(args[1])
