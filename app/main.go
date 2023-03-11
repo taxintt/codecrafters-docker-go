6346cccf3e33606eaeaa89c0669073662fa78ffd
taxintt <nthouse14@gmail.com>
2023-03-11 17:25:20 +0900

    refactoring

--- app/main.go
+++ app/main.go
@@ -1,3 +1,5 @@
+//go:build linux
+
 package main
 
 import (
@@ -5,27 +7,12 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"syscall"
 )
-
-func setupOutput(cmd *exec.Cmd) {
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-
-	go io.Copy(os.Stdout, stdout)
-	go io.Copy(os.Stderr, stderr)
-}
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
@@ -35,41 +22,27 @@ func main() {
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
-	rootDir, err := ioutil.TempDir("", "")
-	if err := os.MkdirAll(filepath.Join(rootDir, filepath.Dir(command)), os.ModeDir); err != nil {
-		os.Exit(1)
+	chrootDir, err := ioutil.TempDir("", "")
+	if err := os.MkdirAll(filepath.Join(chrootDir, filepath.Dir(command)), os.ModeDir); err != nil {
+		log.Fatal(fmt.Errorf("failed to create rootDir: %w", err))
 	}
-	defer os.RemoveAll(rootDir)
+	defer os.RemoveAll(chrootDir)
 
-	src, err := os.Open(command)
-	if err != nil {
-		os.Exit(1)
+	if err = copyExecutableFile(command, chrootDir); err != nil {
+		log.Fatal(fmt.Errorf("failed to copy executable file: %w", err))
 	}
-	srcInfo, err := src.Stat()
-	if err != nil {
-		os.Exit(1)
-	}
-
-	dst, err := os.OpenFile(filepath.Join(rootDir, command), os.O_CREATE|os.O_WRONLY, srcInfo.Mode())
-	if err != nil {
-		os.Exit(1)
-	}
-	if _, err := io.Copy(dst, src); err != nil {
-		os.Exit(1)
-	}
-
-	src.Close()
-	dst.Close()
 
 	// workaround for chroot
-	os.Mkdir(filepath.Join(rootDir, "dev"), os.ModeDir)
-	devnull, _ := os.Create(filepath.Join(rootDir, "/dev/null"))
-	devnull.Close()
-
-	if err = syscall.Chroot(rootDir); err != nil {
+	if err := createDevNullDir(chrootDir); err != nil {
 		fmt.Printf("chroot err: %v", err)
 		os.Exit(1)
 	}
+
+	if err = syscall.Chroot(chrootDir); err != nil {
+		fmt.Printf("chroot err: %v", err)
+		os.Exit(1)
+	}
+
 	cmd := exec.Command(command, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -85,4 +58,49 @@ func main() {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
+}
+
+func copyExecutableFile(command, rootDir string) error {
+	src, err := os.Open(command)
+	if err != nil {
+		return fmt.Errorf("failed to open source file: %w", err)
+	}
+	srcInfo, err := src.Stat()
+	if err != nil {
+		return fmt.Errorf("failed to get source file info: %w", err)
+	}
+
+	dst, err := os.OpenFile(filepath.Join(rootDir, command), os.O_CREATE|os.O_WRONLY, srcInfo.Mode())
+	if err != nil {
+		return fmt.Errorf("failed to open destination file: %w", err)
+	}
+	if _, err := io.Copy(dst, src); err != nil {
+		return fmt.Errorf("failed to copy file from %s to %s: %w", src.Name(), dst.Name(), err)
+	}
+
+	if err := src.Close(); err != nil {
+		return fmt.Errorf("failed to close source file: %w", err)
+	}
+	if err := dst.Close(); err != nil {
+		return fmt.Errorf("failed to close destination file: %w", err)
+	}
+
+	return nil
+}
+
+func createDevNullDir(chrootDir string) error {
+	err := os.Mkdir(filepath.Join(chrootDir, "dev"), os.ModeDir)
+	if err != nil {
+		return fmt.Errorf("failed to create /dev directory: %w", err)
+	}
+
+	devnull, err := os.Create(filepath.Join(chrootDir, "/dev/null"))
+	if err != nil {
+		return fmt.Errorf("failed to create /dev/null file: %w", err)
+	}
+
+	if err := devnull.Close(); err != nil {
+		return fmt.Errorf("failed to close /dev/null file: %w", err)
+	}
+	return nil
 }
